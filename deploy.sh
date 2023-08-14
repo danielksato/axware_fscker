046c97a4a7ddfa4413b37bd6b7c940022cd8e833
Daniel Sato <dksato@gmail.com>
2023-08-14 00:44:31 -0500

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 docker build . -t 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker
 docker push 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker
-ssh -i /home/pretzelface/axware-fscker.pem ec2-user@ec2-3-236-178-34.compute-1.amazonaws.com \
-  docker ps | tail -n 1 | awk '{print $1}' | xargs docker kill \
-  docker pull 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker \
-  docker run -d -p 8080:8080 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker \
+scp -i /home/pretzelface/axware-fscker.pem remoteDeploy.sh ec2-user@ec2-3-236-178-34.compute-1.amazonaws.com:~
+ssh -i /home/pretzelface/axware-fscker.pem ec2-user@ec2-3-236-178-34.compute-1.amazonaws.com "
+sudo chmod +x ./remoteDeploy.sh
+./remoteDeploy.sh
+"
---
+++ remoteDeploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+docker pull 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker
+docker kill `docker ps | tail -n 1 | awk '{print $1}'`
+docker run -d -p 8080:8080 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker
