42dc66d17cec1081b237d4b11572ff5f8d808616
Daniel Sato <dksato@gmail.com>
2023-08-14 11:56:28 -0500

    Handle bad URL

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,14 @@ const fetch = require('node-fetch');
 
 module.exports = function (app) {
   app.use('/proxy', async ({ query: { axware } }, response) => {
-    const axwareRes = await fetch(axware);
-    const html = await axwareRes.text();
-    response.set('content-type', 'text/html');
-    response.send(html);
+    try {
+      const axwareRes = await fetch(axware);
+      const html = await axwareRes.text();
+      response.set('content-type', 'text/html');
+      response.send(html);
+    } catch (e) {
+      response.status(500);
+      response.send(e);
+    }
   });
 };
