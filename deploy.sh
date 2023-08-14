6644bfb6296d6642f3e8586e0d8488b84e415585
Daniel Sato <dksato@gmail.com>
2023-08-13 22:49:31 -0500

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+docker build . -t 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker
+docker push 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker
+ssh -i /home/pretzelface/axware-fscker.pem ec2-user@ec2-3-236-178-34.compute-1.amazonaws.com \
+  docker kill `docker ps | tail -n 1 | awk '{print $1}'` \
+  docker pull 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker \
+  docker run -d -p 8080:8080 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker \
