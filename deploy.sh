cf0e3e950233da012eedaf8e03ac2aef5dca031a
Daniel Sato <dksato@gmail.com>
2023-08-15 09:08:24 -0500

    Reauth on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 917812706670.dkr.ecr.us-east-1.amazonaws.com
 docker build . -t 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker
 docker push 917812706670.dkr.ecr.us-east-1.amazonaws.com/axware-fscker
 scp -i /home/pretzelface/axware-fscker.pem remoteDeploy.sh ec2-user@ec2-3-236-178-34.compute-1.amazonaws.com:~
